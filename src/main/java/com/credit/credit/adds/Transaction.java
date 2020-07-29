b246283fe34f1015899e4e9197a65cd3dff7385a
dmendozy <dmendozy@everis.com>
2020-07-29 15:43:05 -0500

    add Transaction

---
+++ src/main/java/com/credit/credit/adds/Transaction.java
@@ -0,0 +1,17 @@
+package com.credit.credit.adds;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class Transaction {
+    public String transactionId;
+    public String transactionName;
+    public double output;
+    public double input;
+    public double commission;
+    public LocalDate datetime;
+    public String accountId;
+    public String creditId;
+}
