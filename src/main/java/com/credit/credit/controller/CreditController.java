ecd3d8e1adf7a02e06f0375f0bd266f97d1b5e22
dmendozy <67970157+dmendozy@users.noreply.github.com>
2020-08-04 22:57:37 -0500

    add pay credit

--- src/main/java/com/credit/credit/controller/CreditController.java
+++ src/main/java/com/credit/credit/controller/CreditController.java
@@ -1,20 +1,26 @@
 package com.credit.credit.controller;
 
+import com.credit.credit.adds.Transaction;
 import com.credit.credit.model.Credit;
 import com.credit.credit.service.CreditService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/credits")
 public class CreditController {
     @Autowired
     CreditService creditService;
+
+    @Autowired
+    WebClient.Builder webClientBuilder;
 
     @GetMapping
     public Flux<Credit> getAllCredits(){
@@ -47,14 +53,15 @@ public class CreditController {
         return creditService.getByCustomerId(customerId);
     }
 
+    //Search credits between dates
     @GetMapping("/search/{bankId}/{firstDate}/{lastDate}")
-    public Flux<Credit> getAccountsBetweenDates(@PathVariable("bankId") String bankId,
+    public Flux<Credit> getCreditsBetweenDates(@PathVariable("bankId") String bankId,
                                                  @PathVariable("firstDate") String firstDate,
                                                  @PathVariable("lastDate") String lastDate) {
         LocalDate date1 = LocalDate.parse(firstDate);
         LocalDate date2 = LocalDate.parse(lastDate);
         return creditService.getAll()
-                .filter(credit -> credit.getBankId()!=null&&
+                .filter(credit -> credit.getBankId().equals(bankId)&&
                         credit.getCreationDate().compareTo(date1)>=0&&
                         credit.getCreationDate().compareTo(date2)<=0)
                 .flatMap(account -> {
@@ -62,11 +69,50 @@ public class CreditController {
                 });
     }
 
+    //Search credits by expiration date
     @GetMapping("/expiration/{customerId}")
     public Mono getCreditBlocked(@PathVariable("customerId") String id){
-        Mono mono = creditService.getByCustomerId(id)
+        return creditService.getByCustomerId(id)
                 .filter(credit -> credit.getExpirationPayment().compareTo(LocalDate.now())<0)
                 .collectList();
-        return mono;
     }
+
+    //Pay credit
+    @PutMapping("/pay/{creditId}/{amount}")
+    public Mono payCredit(@PathVariable("creditId") String creditId,
+                          @PathVariable("amount") double amount){
+        return creditService.getById(creditId)
+                .flatMap(credit -> {
+                    if (credit.getCreditConsumed()>0){
+                        credit.setCreditAvailable(credit.getCreditAvailable()+amount);
+                        credit.setCreditConsumed(credit.getCreditConsumed()-amount);}
+                        return creditService.save(credit);
+                });
+    }
+
+    //Add credit consumed
+    @PutMapping("/charge/{creditId}/{amount}")
+    public Mono addCreditConsumed(@PathVariable("creditId") String creditId,
+                                  @PathVariable("amount") double amount){
+        return creditService.getById(creditId)
+                .flatMap(credit -> {
+                    if (credit.getCreditAvailable()>0&&credit.getCreditAvailable()>=amount&&
+                            credit.getCreditConsumed()>=0){
+                        credit.setCreditConsumed(credit.getCreditConsumed()+amount);
+                        credit.setCreditAvailable(credit.getCreditAvailable()-amount);
+                    }
+                    Transaction transaction = new Transaction("Add credit consumption",amount, LocalDateTime.now(),creditId);
+                    Mono<Transaction> transactionMono= webClientBuilder
+                            .build()
+                            .post()
+                            .uri("http://localhost:8084/transactions/")
+                            .body(Mono.just(transaction),Transaction.class)
+                            .retrieve()
+                            .bodyToMono(Transaction.class);
+                    return transactionMono.flatMap(t->{
+                        return creditService.save(credit);
+                    });
+                });
+    }
+
 }
