b94b6cbb121026494aa764dd5b72b6a6d8e6cd2e
dmendozy <67970157+dmendozy@users.noreply.github.com>
2020-08-04 22:57:04 -0500

    update

--- src/main/java/com/credit/credit/adds/Transaction.java
+++ src/main/java/com/credit/credit/adds/Transaction.java
@@ -2,16 +2,22 @@ package com.credit.credit.adds;
 
 import lombok.Data;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Data
 public class Transaction {
     public String transactionId;
     public String transactionName;
-    public double output;
-    public double input;
+    public double amount;
     public double commission;
-    public LocalDate datetime;
+    public LocalDateTime dateTime;
     public String accountId;
     public String creditId;
+
+    public Transaction(String transactionName, double amount, LocalDateTime dateTime, String creditId) {
+        this.transactionName = transactionName;
+        this.amount = amount;
+        this.dateTime = dateTime;
+        this.creditId = creditId;
+    }
 }
